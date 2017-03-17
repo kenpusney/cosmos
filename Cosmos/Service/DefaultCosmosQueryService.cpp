dcaaa2f27908462835b32230c5202615709ea3f8
Kimmy Leo <kenpusney@gmail.com>
2017-03-17 15:23:08 +0800

    Fix the compile warning

--- Cosmos/Service/DefaultCosmosQueryService.cpp
+++ Cosmos/Service/DefaultCosmosQueryService.cpp
@@ -2,128 +2,128 @@
 #include "DefaultCosmosQueryService.h"
 
 DefaultCosmosQueryService::DefaultCosmosQueryService(MYSQL *dataSource)
-	: _dataSource(dataSource)
+    : _dataSource(dataSource)
 {
 }
 
-Entity* DefaultCosmosQueryService::QuerySystemStructure(const std::string &name)
+Entity *DefaultCosmosQueryService::QuerySystemStructure(const std::string &name)
 {
-	IdentifiedEntity *root = QueryEntity(name);
-	
-	if (!root)
-		return NULL;
-	
-	if (!QuerySubEntities(*root))
-	{
-		delete root;
-		return NULL;
-	}
+    IdentifiedEntity *root = QueryEntity(name);
 
-	return root;
+    if (!root)
+	return NULL;
+
+    if (!QuerySubEntities(*root))
+    {
+	delete root;
+	return NULL;
+    }
+
+    return root;
 }
 
-Entity* DefaultCosmosQueryService::QuerySystemMembership(const std::string &name)
+Entity *DefaultCosmosQueryService::QuerySystemMembership(const std::string &name)
 {
-	IdentifiedEntity *entity = QueryEntity(name);
+    IdentifiedEntity *entity = QueryEntity(name);
 
-	if (!entity)
-		return NULL;
+    if (!entity)
+	return NULL;
 
-	Entity *top = NULL;
-	int nextId = entity->ID();
-	delete entity;
+    Entity *top = NULL;
+    int nextId = entity->ID();
+    delete entity;
 
-	while (nextId >= 0)
-	{
-		std::stringstream ss;
-		ss << "SELECT NAME, PARENT FROM SYSTEMS WHERE ID=" << nextId;
-
-		if (mysql_query(_dataSource, ss.str().c_str()) != 0)
-			break;
-
-		MYSQL_RES *res = mysql_store_result(_dataSource);
-		if (!res)
-			break;
-
-		MYSQL_ROW row = mysql_fetch_row(res);
-		if (!row)
-		{
-			mysql_free_result(res);
-			break;
-		}
-
-		std::string name = row[0];
-		nextId = row[1] ? atoi(row[1]) : -1;
-		
-		mysql_free_result(res);
-
-		Entity *superEntity = new Entity(name);
-
-		if (top)
-			superEntity->AppendSubEntity(top);
-
-		top = superEntity;
-	}
-
-	return top;
-}
-
-IdentifiedEntity* DefaultCosmosQueryService::QueryEntity(const std::string &name)
-{
-	if (name.find('\'') != std::string::npos)
-		return NULL;
-
+    while (nextId >= 0)
+    {
 	std::stringstream ss;
-	ss << "SELECT ID FROM SYSTEMS WHERE NAME='" << name << "'";
+	ss << "SELECT NAME, PARENT FROM SYSTEMS WHERE ID=" << nextId;
 
 	if (mysql_query(_dataSource, ss.str().c_str()) != 0)
-		return NULL;
+	    break;
 
 	MYSQL_RES *res = mysql_store_result(_dataSource);
 	if (!res)
-		return NULL;
+	    break;
 
 	MYSQL_ROW row = mysql_fetch_row(res);
 	if (!row)
 	{
-		mysql_free_result(res);
-		return NULL;
+	    mysql_free_result(res);
+	    break;
 	}
 
-	int id = atoi(row[0]);
+	std::string name = row[0];
+	nextId = row[1] ? atoi(row[1]) : -1;
+
 	mysql_free_result(res);
 
-	return new IdentifiedEntity(id, name);
+	Entity *superEntity = new Entity(name);
+
+	if (top)
+	    superEntity->AppendSubEntity(top);
+
+	top = superEntity;
+    }
+
+    return top;
+}
+
+IdentifiedEntity *DefaultCosmosQueryService::QueryEntity(const std::string &name)
+{
+    if (name.find('\'') != std::string::npos)
+	return NULL;
+
+    std::stringstream ss;
+    ss << "SELECT ID FROM SYSTEMS WHERE NAME='" << name << "'";
+
+    if (mysql_query(_dataSource, ss.str().c_str()) != 0)
+	return NULL;
+
+    MYSQL_RES *res = mysql_store_result(_dataSource);
+    if (!res)
+	return NULL;
+
+    MYSQL_ROW row = mysql_fetch_row(res);
+    if (!row)
+    {
+	mysql_free_result(res);
+	return NULL;
+    }
+
+    int id = atoi(row[0]);
+    mysql_free_result(res);
+
+    return new IdentifiedEntity(id, name);
 }
 
 bool DefaultCosmosQueryService::QuerySubEntities(IdentifiedEntity &root)
 {
-	std::stringstream ss;
-	ss << "SELECT ID, NAME FROM SYSTEMS WHERE PARENT=" << root.ID();
+    std::stringstream ss;
+    ss << "SELECT ID, NAME FROM SYSTEMS WHERE PARENT=" << root.ID();
 
-	if (mysql_query(_dataSource, ss.str().c_str()) != 0)
-		return false;
+    if (mysql_query(_dataSource, ss.str().c_str()) != 0)
+	return false;
 
-	MYSQL_RES *res = mysql_store_result(_dataSource);
-	if (!res)
-		return NULL;
+    MYSQL_RES *res = mysql_store_result(_dataSource);
+    if (!res)
+	return false;
 
-	MYSQL_ROW row;
-	while ((row = mysql_fetch_row(res)))
+    MYSQL_ROW row;
+    while ((row = mysql_fetch_row(res)))
+    {
+	int id = atoi(row[0]);
+	std::string name = row[1];
+
+	IdentifiedEntity *entity = new IdentifiedEntity(id, name);
+	root.AppendSubEntity(entity);
+
+	if (!QuerySubEntities(*entity))
 	{
-		int id = atoi(row[0]);
-		std::string name = row[1];
-
-		IdentifiedEntity *entity = new IdentifiedEntity(id, name);
-		root.AppendSubEntity(entity);
-
-		if (!QuerySubEntities(*entity))
-		{
-			mysql_free_result(res);
-			return false;
-		}
+	    mysql_free_result(res);
+	    return false;
 	}
+    }
 
-	mysql_free_result(res);
-	return true;
+    mysql_free_result(res);
+    return true;
 }
